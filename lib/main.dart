59e2fb364a5f2b592f0ccf66773f6652ae8274f3
oriohac <oriohac@gmail.com>
2024-07-12 19:37:15 +0100

    Start app from screencontroller

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:shopatsharrie/Screens/home.dart';
+import 'package:shopatsharrie/Screens/screencontroller.dart';
 
 Future<void> main() async {
   if (kReleaseMode) {
@@ -21,7 +22,7 @@ class Shopatsharrie extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home:Screencontroller(),
     );
   }
 }
